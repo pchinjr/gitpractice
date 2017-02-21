54f80914d4ed0d72768be041ab47dcfef13eabef
Paul Chin <pchinjr@gmail.com>
2017-02-21 14:47:01 -0500

    second fix

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ console.log("hello")
 
 console.log("button here for main function")
 
-console.log("some fix");
+console.log("some fix")
 
-console.log("new feature");
+console.log("new feature")
 
 console.log("a second new feature")
 
+console.log("second fix")

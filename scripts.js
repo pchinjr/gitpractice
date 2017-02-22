8ae6530813d2761db87f0d817ae582d32727632a
Paul Chin <pchinjr@gmail.com>
2017-02-21 23:24:07 -0500

    project setup

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function initElement() {
+    var b = document.getElementById("btn");
+    b.onclick = showAlert;
+};
+
+function showAlert(event) {
+    alert("HOW'D IT GET BURNED?!?!HOW'D IT GET BURNED?!?!HOW'D IT GET BURNED?!?!HOW'D IT GET BURNED?!?!");
+}
